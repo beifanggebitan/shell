dd270f7dd1a6bb888fca1180e402bd799dafe3ce
beifanggebitan <57257281+beifanggebitan@users.noreply.github.com>
2019-11-06 21:35:56 +0300

    Update Autoback-xtraback.sh

--- 脚本实用工具/Autoback-xtraback.sh
+++ 脚本实用工具/Autoback-xtraback.sh
@@ -19,16 +19,16 @@ function getdir()         #指定函数名，也可以fun()直接进行，但是
 function backup()         #创建另一个函数，用于执行备份
 {
 	if [ ${week} == "0" ];then   #如果今天是本周第一天（即英国人习惯的周日是美洲第一天）
-		$cmd --defaults-file=${mycnf} --user=${user} --password=${passwd} ${datadir}&>${logdir}/${Time}-log
-		[ $? -eq 0 ] && stat=`tail -1 ${logdir}/${Time}-log |awk '{print $4}'`
-		if [ "${stat}" == "OK!" ];then
-			echo "${Time} mysql backup is success!"
+		$cmd --defaults-file=${mycnf} --user=${user} --password=${passwd} ${datadir}&>${logdir}/${Time}-log  #这个是备份命令的执行参数
+		[ $? -eq 0 ] && stat=`tail -1 ${logdir}/${Time}-log |awk '{print $4}'`   #通过与或执行简单的判断，如果上面的执行成功的，就会得到相应的日志，，aek进行空格分段，取第四段，这个可以检查awk用法
+		if [ "${stat}" == "OK!" ];then        #如果上面那条命令获取的stat参数是OK的话
+			echo "${Time} mysql backup is success!" #打印时间和这句成功的话
 		else
-			echo "${Time} mysql backup is fail! please check ${logdir}/${Time}-log"
+			echo "${Time} mysql backup is fail! please check ${logdir}/${Time}-log"  #如果不是的话，就说明执行错误
 		fi
 	else
 		$cmd --defaults-file=${mycnf} --user=${user} --password=${passwd} --incremental --incremental-basedir=${datadir}&>${logdir}/${Time}-log
-					
+	        #如果不是本周第一天，就按照这一行参数执行备份命令
 }
 
 shell讲解：
