28ee73f650f895013904527f40a6f5f1f71946ab
beifanggebitan <57257281+beifanggebitan@users.noreply.github.com>
2020-02-28 21:23:37 +0300

    Update 从函数返回数组.sh

--- 脚本函数/从函数返回数组.sh
+++ 脚本函数/从函数返回数组.sh
@@ -3,13 +3,13 @@
 # returning an array value
 
 function arraydblr {
-	local origarry
+	local origarry  #local可以在函数内定义局部变量，显示局部变量，也可以在函数内定义全局变量
 	local newarray
 	local elements
 	local i
-	origarry=(`echo "$@"`)
+	origarry=(`echo "$@"`) #$@所有参数列表。如"$@"用「"」括起来的情况、以"$1" "$2" … "$n" 的形式输出所有参数
 	newarray=(`echo "$@"`)
-	elements=$[ $# - 1 ]
+	elements=$[ $# - 1 ] #$#表示添加到Shell的参数个数
 	for (( i=0; i<=$elements; i++ ))
 	{
 		newarray[$i]=$[ ${origarry[$i]} * 2 ]
